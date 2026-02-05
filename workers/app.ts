34957087a974f80b76c851c41d344229060abb62
Ryan Luu <ryan.luu@ryanluu.dev>
2026-02-05 14:57:03 -0800

    Fix ordering for routes
    Signed-off-by: Ryan Luu <ryan.luu@ryanluu.dev>

--- workers/app.ts
+++ workers/app.ts
@@ -4,26 +4,24 @@ import { createRequestHandler } from "react-router";
 
 const app = new Hono<{ Bindings: Env }>();
 
-// Add more routes here
-
-app.get("*", (c) => {
-	const requestHandler = createRequestHandler(
-		() => import("virtual:react-router/server-build"),
-		import.meta.env.MODE,
-	);
-
-	return requestHandler(c.req.raw, {
-		cloudflare: { env: c.env, ctx: c.executionCtx },
-	});
-});
-
 // Secure all the API routes
 app.use('/api/*', (c, next) => {
-	const jwtMiddleware = jwt({
-		secret: c.env.JWT_SECRET,
-		alg: 'HS256',
-	})
-	return jwtMiddleware(c, next)
+  const jwtMiddleware = jwt({
+    secret: c.env.JWT_SECRET,
+    alg: 'HS256',
+  })
+  return jwtMiddleware(c, next)
 })
+
+app.get("*", (c) => {
+  const requestHandler = createRequestHandler(
+    () => import("virtual:react-router/server-build"),
+    import.meta.env.MODE,
+  );
+
+  return requestHandler(c.req.raw, {
+    cloudflare: { env: c.env, ctx: c.executionCtx },
+  });
+});
 
 export default app;
