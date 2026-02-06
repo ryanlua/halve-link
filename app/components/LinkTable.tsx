10eabc99495e81e142a8a0307563d0e25ca315e2
Ryan Luu <ryan.luu@ryanluu.dev>
2026-02-06 15:33:31 -0800

    Wrap in card
    Signed-off-by: Ryan Luu <ryan.luu@ryanluu.dev>

--- app/components/LinkTable.tsx
+++ app/components/LinkTable.tsx
@@ -1,4 +1,4 @@
-import { Button, Table } from "@cloudflare/kumo";
+import { Button, Table, LayerCard } from "@cloudflare/kumo";
 import { TrashIcon } from "@phosphor-icons/react";
 
 const linkData = [
@@ -12,33 +12,37 @@ const linkData = [
 
 export function LinkTable() {
   return (
-    <Table>
-      <Table.Header>
-        <Table.Row>
-          <Table.CheckHead />
-          <Table.Head>Slug</Table.Head>
-          <Table.Head>Link</Table.Head>
-          <Table.Head>By</Table.Head>
-          <Table.Head>Date</Table.Head>
-          <Table.Head></Table.Head>
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-        {linkData.map((row) => (
-          <Table.Row key={row.slug}>
-            <Table.CheckCell />
-            <Table.Cell>{row.slug}</Table.Cell>
-            <Table.Cell>{row.link}</Table.Cell>
-            <Table.Cell>{row.by}</Table.Cell>
-            <Table.Cell>{row.date}</Table.Cell>
-            <Table.Cell className="text-right">
-              <Button variant="ghost" size="sm" shape="square">
-                <TrashIcon weight="bold" size={16} />
-              </Button>
-            </Table.Cell>
-          </Table.Row>
-        ))}
-      </Table.Body>
-    </Table>
+    <LayerCard>
+      <LayerCard.Primary className="p-0">
+        <Table>
+          <Table.Header>
+            <Table.Row>
+              <Table.CheckHead />
+              <Table.Head>Slug</Table.Head>
+              <Table.Head>Link</Table.Head>
+              <Table.Head>By</Table.Head>
+              <Table.Head>Date</Table.Head>
+              <Table.Head></Table.Head>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {linkData.map((row) => (
+              <Table.Row key={row.slug}>
+                <Table.CheckCell />
+                <Table.Cell>{row.slug}</Table.Cell>
+                <Table.Cell>{row.link}</Table.Cell>
+                <Table.Cell>{row.by}</Table.Cell>
+                <Table.Cell>{row.date}</Table.Cell>
+                <Table.Cell className="text-right">
+                  <Button variant="ghost" size="sm" shape="square">
+                    <TrashIcon weight="bold" size={16} />
+                  </Button>
+                </Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
+      </LayerCard.Primary>
+    </LayerCard>
   );
 }
