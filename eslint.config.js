8f9586544e5f6f631a7db01a296dea6da978b511
Ryan Luu <ryan.luu@ryanluu.dev>
2026-02-08 23:17:40 -0800

    Add ESLint
    Signed-off-by: Ryan Luu <ryan.luu@ryanluu.dev>

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import eslint from "@eslint/js";
+import { defineConfig, globalIgnores } from "eslint/config";
+import prettierConfig from "eslint-config-prettier";
+import tseslint from "typescript-eslint";
+
+export default defineConfig(
+	globalIgnores([
+		"dist",
+		".wrangler",
+		".react-router/",
+		"worker-configuration.d.ts",
+	]),
+	eslint.configs.recommended,
+	tseslint.configs.recommended,
+	prettierConfig
+);
